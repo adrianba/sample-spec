110dfdb114218ae6c52df5414aee65d54e36db30
Ade Bateman <bateman@acm.org>
2016-02-05 11:33:07 -0800

    Enable publication

--- .deploy-output.sh
+++ .deploy-output.sh
@@ -13,4 +13,4 @@ git status
 
 GH_REPO="@github.com/adrianba/sample-spec-draft.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
-#git push --force --quiet $FULL_REPO master:gh-pages > /dev/null 2>&1
+git push --force --quiet $FULL_REPO master:gh-pages > /dev/null 2>&1
